e20b5b0ee6d7a452983fdc1a69601d63af7c0421
MrATsoy <113983576+MrATsoy@users.noreply.github.com>
2023-12-14 00:25:10 +0600

    Add files via upload

---
+++ TFIproj/script.js
@@ -0,0 +1,53 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("faqToggle1").addEventListener("click", playAudio);
+document.getElementById("faqToggle2").addEventListener("click", playAudio);
+
+function playAudio() {
+  var audio = document.getElementById("myAudio");
+  audio.play();
+}
